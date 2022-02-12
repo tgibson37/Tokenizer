dfe42fa815dd90418547b94ad4dcd42ece669dc2
Tom Gibson <tgibson37@gmail.com>
2022-02-12 11:53:24 -0500

    tidied, returns ArrayList of tokens

--- Token.java
+++ Token.java
@@ -3,40 +3,37 @@ import java.util.*;
 public class Token {
 	public static final int ALPHA = 1000;
 	public static final int NUM = 2000;
-// returns number of tokens
+	private static int cursor=0;
+
 	public static List<String> tokenize(String line) {
 		int nxtTok;
 		int tlen = line.length();
-		int cursor = 0;
-		int first,last;
 		ArrayList<String> tokens = new ArrayList<String>();
 		while(cursor<tlen-1) {
-			first = cursor;
-			last = getTokLast(line,first);
-			if(last>=first) {
-				String token = line.substring(first,last);
-				tokens.add(token);
-				cursor = skip(line,last+1,' ',tlen);
+			String tok = getTok(line,cursor);
+			if(tok!=null) {
+				tokens.add(tok);
+				cursor += tok.length();
 			}
 			else return tokens;
 		}
 		return tokens;
 	}
 	
-	private static int skip(String line, int cursor, char separator, int tlen){
-		while(cursor<tlen && line.charAt(cursor)==separator)++cursor;
-		return cursor;
+	private static boolean isWhite(String line, int cursor) {
+		return Character.isWhitespace(line.charAt(cursor)); 
 	}
 	
-	private static int getTokLast(String line, int first) {
-		int last = first+1;   // have at least one already
-		int tlen = line.length();
+	private static String getTok(String line, int first) {
+		while(isWhite(line,first)){++first;++cursor;}
 		int type = setType(line.charAt(first));
+		int last = first+1;
+		int tlen = line.length();
 		while(last < tlen) {
-			if( isOfType(line.charAt(last),type) ) ++last;   // include this char
-			else break;                                     // no, done
+			if( isOfType(line.charAt(last),type) ) ++last;
+			else break;
 		} 
-		return last;
+		return line.substring(first,last);
 	}
 
 	private static boolean isOfType(char x, int type) {
@@ -61,10 +58,11 @@ public class Token {
 	public static void main(String args[]){
 		int TOKLEN = 10;
 		String tok[] = new String[TOKLEN];
-		String dothis = "foo 30 bar";
+		String dothis = "  foo 30 bar X17 ";
 		List<String> tokens = tokenize(dothis);
 		System.out.println(dothis);
 		int toklen = tokens.size();
 		for(int i=0; i<toklen; ++i) System.out.println(tokens.get(i));
 	}
 }
+//System.out.println("~16 cursor,toklen = "+cursor+" "+tok.length() );
